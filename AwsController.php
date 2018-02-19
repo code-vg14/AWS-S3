253bc163a3aa758a88246d1e269967f55f1a2dd5
vivekgautam14 <36264702+vivekgautam14@users.noreply.github.com>
2018-02-19 10:58:49 +0530

    Update AwsController.php
    You need AWS SDK to work with this code. You can read instructions to install it from this link - https://docs.aws.amazon.com/aws-sdk-php/v3/guide/getting-started/installation.html
     
    To get preassigned urls for your S3 content you need to call this getS3Content(). Pass your content url as parameter
    
                            $path= 'path to your file';                      // '/folder/imagename.png'
    			$s3Content = new AwsController();
    			$file = $s3Content->getS3Content($path);

--- AwsController.php
+++ AwsController.php
@@ -10,84 +10,16 @@ use Cake\Core\Configure;
 use Cake\Event\Event;
 
 /**
- * Courses Controller
- *
- * @property \App\Model\Table\CoursesTable $Courses
+ * @property \App\Model\Table\
  */
 class AwsController extends AppController
 {
-	public function beforeFilter(Event $event){
-	
+	public function beforeFilter(Event $event){	
 		parent::beforeFilter($event);
-		$this->Auth->allow('getCloudFrontContent');
-	}
-	public function getCloudFrontObject(){
-		
-		try{
-			$this->autoRender = false;
-			$cfCredentials = Configure::read('CLOUDFRONT_CREDENTIALS');
-			$cloudFront = new CloudFrontClient([
-					'region'  => $cfCredentials['REGION'],
-					'version' => $cfCredentials['VERSION']
-			]);	
-				
-			return $cloudFront;
-			
-		}catch (CloudFrontException $e) {
-			echo $e->getMessage() . "\n";
-		}
-	}
-	
-	public function getCloudFrontContent($path = null){
-		
-		try{
-			$this->autoRender = false;
-			if(isset($this->request->data['path'])){
-				$path = $this->request->data['path'];			
-			}	
-		//	$path = '/images/outlook-2016-capacitacion-large.png';		
-			$cloudFront = $this->getCloudFrontObject();
-			$cfCredentials = Configure::read('CLOUDFRONT_CREDENTIALS');			
-			$expires = time() + 10;
-			//$privateKey = $this->getS3Content($cfCredentials['PRIVATE_KEY']);			
-			// Create a signed URL for the resource using the canned policy
-			$signedUrlCannedPolicy = $cloudFront->getSignedUrl([
-					'url'         => $cfCredentials['CF_CONTENT_URL'].$path,
-					'expires'     => $expires,
-					'private_key' => WWW_ROOT.'/'.$cfCredentials['PRIVATE_KEY'],
-					'key_pair_id' => $cfCredentials['KEY_PAIR']
-			]);			
-			if ($this->request->is('ajax') &&  isset($this->request->data['path']))	{
-				echo json_encode(['url'=>$signedUrlCannedPolicy]);
-			}else{
-				
-				return $signedUrlCannedPolicy;
-			}			
-			
-		}catch (CloudFrontException $e) {
-			echo $e->getMessage() . "\n";
-		}
-		
-	}
-	
-	
-	public function checkFileExistsOnS3($path){
-		
-		try{			
-			$this->autoRender = false;			
-			$s3Credentials = Configure::read('S3_CREDENTIALS');
-			$s3 = $this->getS3Object();
-			$response = $s3->doesObjectExist($s3Credentials['BUCKET'],$path);
-			return $response;	
-		}catch (S3Exception $e) {
-			echo $e->getMessage() . "\n";
-		}		
-	}
-	
-	public function getS3Object(){
-	
-		try {
-	
+	}		
+	//** This will get the S3 Object *//
+	public function getS3Object(){	
+		try {	
 			$this->autoRender = false;
 			$s3Credentials = Configure::read('S3_CREDENTIALS');
 	
@@ -108,14 +40,8 @@ class AwsController extends AppController
 		}
 	
 	}
-	
-	public function getS3Content($path = null){
-	
-	
-		$filepath = WWW_ROOT.'reference-pdf-files/a2.pdf';
-		$root = '/tmp/';
-		$prefix = null;
-		$temp	= $root . $prefix . mt_rand() . '.pdf';
+	//** Get S3 Preassigned urls for your content**//
+	public function getS3Content($path = null){	
 		try{
 			$s3Credentials = Configure::read('S3_CREDENTIALS');
 			$s3 = $this->getS3Object();
@@ -126,18 +52,12 @@ class AwsController extends AppController
 			));
 			$cmd = $s3->getCommand('GetObject', [
 					'Bucket' => $s3Credentials['BUCKET'],
-					'Key'    => $path,
-						
+					'Key'    => $path,						
 	
 			]);
-				
-	
-	
+					
 			$request = $s3->createPresignedRequest($cmd, '+20 minutes');
 			$presignedUrl = (string)$request->getUri();
-			/* 	$stream_wrapper = new StreamWrapper();
-				$stream_wrapper->register($s3);
-				$s3->registerStreamWrapper(); */
 			return $temp;
 	
 		}catch (S3Exception $e) {
